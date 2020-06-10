86f7cbacc800406d24d124b5a81db5f1fad969d6
Kristi Kacaj <kristik.web@gmail.com>
2020-06-10 12:19:17 -0400

    created variables for packages and user input. Added switch statement to execute command

--- liri.js
+++ liri.js
@@ -1,3 +1,33 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var Spotify = require("node-spotify-api");
+
+//Create variable to access the keys.js file containing the API keys
+let keys = require("./keys.js");
+//Create variables for the packages
+let Spotify = require("node-spotify-api");
+let axios = require("axios");
+let fs = require("fs");
+let moment = require("moment");
+
+//Variables for the arguments to be inputted by the user
+let command = process.argv[2];
+const userInput =process.argv.slice().join(" ")
+
+//Use switch statement to determine which command is executed
+function liriRun (command, userInput) {
+    switch (command) {
+        case "concert-this":
+            getConcert(userInput);
+            break;
+        case "spotify-this-song":
+            getSpotify(userInput);
+            break;
+        case "movie-this":
+            getMovie(userInput);
+            break;
+        case "do-what-it-says":
+            doIt();
+            break;
+        default:
+            console.log("Please try again.")
+    }
+};
