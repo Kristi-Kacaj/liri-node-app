d4ce186b9d1bca270e014cb8c15fb4d9419fccd5
Kristi Kacaj <kristik.web@gmail.com>
2020-06-10 15:13:04 -0400

    Created functions for omdb api and do-what-it-says

--- liri.js
+++ liri.js
@@ -10,12 +10,13 @@ let moment = require("moment");
 
 //Variables for the arguments to be inputted by the user
 let command = process.argv[2];
-let userInput = process.argv.slice().join(" ")
+let userInput = process.argv.slice(3).join(" ")
 
 //Use switch statement to determine which command is executed
 function liriRun (command, userInput) {
     switch (command) {
         case "concert-this":
+            console.log(userInput);
             getConcert(userInput);
             break;
         case "spotify-this-song":
@@ -39,7 +40,7 @@ function getConcert(artist) {
     axios.get(concertQueryURL).then(function (response) {
         console.log("Name of the Venue: " + response.data[0].venue.name + "\r\n");
         console.log("Venue Location: " + response.data[0].venue.city + "\r\n");
-        console.log("Date of Event: " + moment(response.data[0].datatime).format("MM-DD-YYYY") + "\r\n");
+        console.log("Date of Event: " + moment(response.data[0].datetime).format("MM-DD-YYYY") + "\r\n");
     })
 };
 
@@ -65,3 +66,40 @@ function getSpotify(song) {
         console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
     });
 };
+
+//Function to search OMDB API
+function getMovie(movie) {
+    if (!movie) {
+        movie = "Mr.Nobody";
+    }
+
+    let movieQueryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+    axios.request(movieQueryURL).then(
+        function (response) {
+            console.log("Title: " + response.data.Title + "\r\n");
+            console.log("Year Realeased:  " + response.data.Year + "\r\n");
+            console.log("IMDB Rating: " + response.data.imdbRating + "\r\n");
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\r\n");
+            console.log("Country Produced: " + response.data.Country + "\r\n");
+            console.log("Language: " + response.data.Language + "\r\n");
+            console.log("Plot: " + response.data.Plot + "\r\n");
+            console.log("Actors: " + response.data.Actors + "\r\n");
+        });
+};
+
+//Function to take data from .txt file and send to another function when user enters "do-what-it-says"
+function doIt() {
+    fs.readFile("random.txt", "utf-8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log(data);
+
+            let random = data.split(",");
+            liriRun(random[0], random[1]);
+        }
+    });
+};
+
+liriRun(command, userInput);
