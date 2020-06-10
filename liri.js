d0e13485cd68be19ba209c550891e8fab4e65df2
Kristi Kacaj <kristik.web@gmail.com>
2020-06-10 13:11:14 -0400

    Created function for spotify api

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ let moment = require("moment");
 
 //Variables for the arguments to be inputted by the user
 let command = process.argv[2];
-const userInput =process.argv.slice().join(" ")
+let userInput = process.argv.slice().join(" ")
 
 //Use switch statement to determine which command is executed
 function liriRun (command, userInput) {
@@ -34,12 +34,34 @@ function liriRun (command, userInput) {
 
 //Function to search Bands in Town API
 function getConcert(artist) {
-    let artist = userInput;
-    let concertQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    let concertQueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
     axios.get(concertQueryURL).then(function (response) {
         console.log("Name of the Venue: " + response.data[0].venue.name + "\r\n");
         console.log("Venue Location: " + response.data[0].venue.city + "\r\n");
         console.log("Date of Event: " + moment(response.data[0].datatime).format("MM-DD-YYYY") + "\r\n");
     })
+};
+
+//Function for Spotify API
+function getSpotify(song) {
+    let spotify = new Spotify(keys.spotify);
+
+    if (!song) {
+        song = "The Sign";
+    };
+
+    spotify.search({
+        type: "track",
+        query: song
+    }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+
+        console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name + "\r\n");
+        console.log("Song Name: " + data.tracks.items[0].name + "\r\n");
+        console.log("Preview Link: " + data.tracks.items[0].href + "\r\n");
+        console.log("Album: " + data.tracks.items[0].album.name + "\r\n");
+    });
 };
